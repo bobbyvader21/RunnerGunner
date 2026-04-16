3a198518f98219d2d30ad05b33c2e27eadab0743
Roberto Cuellar III <bobbycuellariii@gmail.com>
2026-04-16 01:21:02 -0500

    Added a grounding gizmo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,13 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpVelocity = 5;
     [SerializeField] private float _jumpDuration = 0.5f;
 
-    // Start is called before the first frame update
-    void Start()
+    void OnDrawGizmos()
     {
-        
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        float bottomY = spriteRenderer.bounds.extents.y;
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - bottomY);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 
     // Update is called once per frame
