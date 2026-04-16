91d11a3734a3a7e0e2e9fedf77eef1c49609dd87
Roberto Cuellar III <bobbycuellariii@gmail.com>
2026-04-16 00:37:28 -0500

    Added player left, right and jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Updated at " + Time.time);
+        var horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        var vertical = rb.velocity.y;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            vertical = 5;
+        }
+        rb.velocity = new Vector2(horizontal, vertical);
     }
 }
